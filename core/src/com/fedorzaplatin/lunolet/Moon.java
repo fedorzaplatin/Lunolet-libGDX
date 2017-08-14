14c4500c0b9e1bbfc055aa83aa01b1c18b233089
Fedor Zaplatin <fedor.zaplatin@gmail.com>
2017-08-14 13:07:34 +0500

    Added required methods to the moon class

--- core/src/com/fedorzaplatin/lunolet/Moon.java
+++ core/src/com/fedorzaplatin/lunolet/Moon.java
@@ -19,25 +19,26 @@ public class Moon extends Actor{
     private Body body;
     private Fixture fixture;
     private PolygonRegion polygonRegion;
+    private float[] vertices;
+    private TextureRegion moonTexture;
 
     public Moon(World world, Texture texture, Vector2 position) {
         this.world = world;
         position.y = position.y + 100 / PPM; //Lift the surface a little bit
 
         texture.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat);
-        TextureRegion textureRegion = new TextureRegion(texture);
+        moonTexture = new TextureRegion(texture);
 
-        float[] vertexes;
-        vertexes = generateSurface();
-        short[] triangles = new EarClippingTriangulator().computeTriangles(vertexes).toArray();
+        vertices = generateSurface();
+        short[] triangles = new EarClippingTriangulator().computeTriangles(vertices).toArray();
 
-        this.polygonRegion = new PolygonRegion(textureRegion, vertexes, triangles);
+        this.polygonRegion = new PolygonRegion(moonTexture, vertices, triangles);
         BodyDef def = new BodyDef();
         def.position.set(position);
         def.type = BodyDef.BodyType.StaticBody;
         body = world.createBody(def);
         ChainShape shape = new ChainShape();
-        shape.createChain(vertexes);
+        shape.createChain(vertices);
         fixture = body.createFixture(shape, 0);
         fixture.setUserData("moon");
         fixture.setFriction(0.7f);
@@ -78,12 +79,53 @@ public class Moon extends Actor{
             vertexes[i + 1] = y / PPM;
             vertexes[i + 2] = (x + 90) / PPM;
             vertexes[i + 3] = y / PPM;
-            x += (54 + 90);
+            x += 54 + 90;
         }
 
         //Set end vertex
         vertexes[26] = 800 / PPM;
         vertexes[27] = 0;
         return vertexes;
+    }
+
+    public void generateLeft() {
+        Random rand = new Random();
+        float[] newVertices = new float[vertices.length + 4];
+        System.arraycopy(vertices, 2, newVertices, 6, vertices.length - 2);
+        float x = vertices[2] * PPM - 54;
+        float y = rand.nextInt(131) + 20f;
+
+        newVertices[4] = x / PPM;
+        newVertices[5] = y / PPM;
+        newVertices[2] = (x - 90) / PPM;
+        newVertices[3] = y / PPM;
+        newVertices[0] = (x - 90) / PPM;
+        newVertices[1] = 0;
+
+        vertices = newVertices;
+        this.update();
+    }
+
+    public void generateRight() {
+        Random rand = new Random();
+        float[] newVertices = new float[vertices.length + 4];
+        System.arraycopy(vertices, 0, newVertices, 0, vertices.length - 2);
+        float x = vertices[vertices.length - 3] * PPM + 54;
+        float y = rand.nextInt(131) + 20f;
+
+        newVertices[vertices.length - 1] = x / PPM;
+        newVertices[vertices.length] = y / PPM;
+        newVertices[vertices.length + 1] = (x + 90) / PPM;
+        newVertices[vertices.length + 2] = y / PPM;
+        newVertices[vertices.length + 3] = (x + 90) / PPM;
+        newVertices[vertices.length + 4] = 0;
+
+        vertices = newVertices;
+        this.update();
+    }
+
+    private void update() {
+        short[] triangles = new EarClippingTriangulator().computeTriangles(vertices).toArray();
+        this.polygonRegion = new PolygonRegion(moonTexture, vertices, triangles);
     }
 }
