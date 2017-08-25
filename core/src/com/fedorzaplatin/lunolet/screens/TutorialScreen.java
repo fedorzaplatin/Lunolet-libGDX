6e215225d8088eed61efad82b6ce11e5854d3ae5
Fedor Zaplatin <fedor.zaplatin@gmail.com>
2017-08-25 08:59:18 +0500

    Added tutorial screen class

---
+++ core/src/com/fedorzaplatin/lunolet/screens/TutorialScreen.java
@@ -0,0 +1,53 @@
+package com.fedorzaplatin.lunolet.screens;
+
+import com.fedorzaplatin.lunolet.MainClass;
+
+/**
+ * Tutorial screen for the game
+ */
+
+public class TutorialScreen extends BaseScreen{
+
+    /**
+     * Constructor
+     * @param game main class that extends class Game
+     */
+    public TutorialScreen(MainClass game) {
+        super(game);
+    }
+
+    @Override
+    public void show() {
+        super.show();
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+    }
+
+    @Override
+    public void pause() {
+        super.pause();
+    }
+
+    @Override
+    public void resume() {
+        super.resume();
+    }
+
+    @Override
+    public void hide() {
+        super.hide();
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+    }
+}
