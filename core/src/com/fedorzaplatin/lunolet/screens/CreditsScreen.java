ba919117bf8dabe044e78e69d47f05e491e17921
Fedor Zaplatin <fedor.zaplatin@gmail.com>
2017-08-30 17:04:52 +0500

    Credits screen updated

--- core/src/com/fedorzaplatin/lunolet/screens/CreditsScreen.java
+++ core/src/com/fedorzaplatin/lunolet/screens/CreditsScreen.java
@@ -1,14 +1,17 @@
 package com.fedorzaplatin.lunolet.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.fedorzaplatin.lunolet.MainClass;
 import com.fedorzaplatin.lunolet.ui.LunoletButtonsStyle;
@@ -24,10 +27,22 @@ public class CreditsScreen extends BaseScreen {
         float height = Gdx.graphics.getHeight();
         stage = new Stage(new FitViewport(width, height));
 
-        Image background = new Image((Texture) game.am.get("credits-screen/background.png"));
+        Label.LabelStyle labelStyle;
+        labelStyle = new Label.LabelStyle((BitmapFont) game.am.get("fonts/bebas52.fnt"), Color.WHITE);
+        Label creditsLabel = new Label("Credits", labelStyle);
+
+        labelStyle = new Label.LabelStyle((BitmapFont) game.am.get("fonts/courierNew30.fnt"), Color.WHITE);
+        String labelText = "Developer\n" +
+                "Fedor Zaplatin\n\n" +
+                "Music\n" +
+                "Borrtex - Our Home\n" +
+                "Parvus Decree - Space Travel\n\n" +
+                "Source code\n" +
+                "github.com/fedorzaplatin/lunolet-libgdx";
+        Label text = new Label(labelText, labelStyle);
+        text.setAlignment(Align.center);
 
         ImageButton backBtn = new ImageButton(new LunoletButtonsStyle((TextureAtlas) game.am.get("buttons.atlas"), "back"));
-        backBtn.setPosition(346, 49);
         backBtn.addCaptureListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
@@ -35,8 +50,14 @@ public class CreditsScreen extends BaseScreen {
             }
         });
 
-        stage.addActor(background);
-        stage.addActor(backBtn);
+        Table table = new Table();
+        table.setFillParent(true);
+        table.add(creditsLabel).pad(26).expandX().center().top();
+        table.row().expand();
+        table.add(text).center();
+        table.row().expand();
+        table.add(backBtn).pad(23).expandX().bottom().left();
+        stage.addActor(table);
     }
 
     @Override
@@ -46,7 +67,7 @@ public class CreditsScreen extends BaseScreen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1,1,1,1);
+        Gdx.gl.glClearColor(3 / 255, 4 / 255,3 / 255, 0);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         stage.act();
